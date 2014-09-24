d99a5f990d27e0569f5ad68452175a46519e0ad6
Amelia Winger-Bearskin <ameliawb@nyu.edu>
2014-09-24 12:30:44 -0400

    trying to learn jquery

--- scripts/iconomation.js
+++ scripts/iconomation.js
@@ -1,31 +1,9 @@
-// add icons
-$('.playspace').append('<i class="em em-fountain"></i>');
-$('.playspace').append('<i class="em em-goat"></i>');
-
-
-// set the initial positions
-
-$('.em-fountain').css({
-  top: 100,
-  left: 300
+$(document).ready(function()
+  {
+  $("#button").click(function(){
+    $("#box").animate({height:"300px"});
+  });
+  $("#btn2").click(function(){
+    $("#box").animate({height:"100px"});
+  });
 });
-
-$('.em-goat').css({
-  top: 110,
-  left: 100
-});
-
-
-// play!
-
-$('.em-fountain').animate({
-  left: 70
-}, 1000);
-
-// after 660ms...
-setTimeout(function() {
-  // ...move the ball
-  $('.em-goat').animate({
-    right: 200
-  }, 800, 'easeOutQuart');
-}, 1000);
