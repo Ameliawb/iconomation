052979a06e7ea967f496457be6c39cd159128f3e
Amelia Winger-Bearskin <ameliawb@nyu.edu>
2014-09-24 21:07:44 -0400

    omg I'm so deep, i got it to partly work

---
+++ iconomation.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+        $(".button").click(function() {
+            $(".box").animate({
+                height: "400px"
+            });
+        });
+        $(".button2").click(function() {
+            $(".box").animate({
+                height: "50px"
+            });
+        });
+         });
