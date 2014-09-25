43677d07fedb1fcfdefb630452c3c4521334e5a3
Amelia Winger-Bearskin <ameliawb@nyu.edu>
2014-09-24 21:31:42 -0400

    Ok shaping up, its silly but I like it.

--- iconomation.js
+++ iconomation.js
@@ -1,12 +1,53 @@
 $(document).ready(function() {
         $(".button").click(function() {
-            $(".box").animate({
+            $(".green").animate({
+                height: "200px"
+            });
+            $(".blue").animate({
+                height: "300px"
+            });
+            $(".red").animate({
                 height: "400px"
             });
         });
         $(".button2").click(function() {
-            $(".box").animate({
-                height: "50px"
+            $("div").animate({
+                width: "100px"
+            });
+            $("div").animate({
+                height: "100px"
             });
         });
-         });
+
+        $(".button3").click(function() {
+            $(".green").animate({
+                width: "400px"
+            });
+            $(".blue").animate({
+                width: "500px"
+            });
+            $(".red").animate({
+                width: "600px"
+            });
+        });
+        $(".button4").click(function() {
+            $("div").animate({
+                width: "100px"
+            });
+             $("div").animate({
+                height: "100px"
+            });
+             $(".button5").click(function() {
+            $("div").animate({
+                  left:'250px',
+                 opacity:'0.5',
+                 height:'150px',
+                 width:'150px'
+            });
+             $("div").animate({
+                height: "100px"
+            });
+        });
+                });
+   });
+
